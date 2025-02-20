7905977fae5efe8fdecc3a476fad9cb1332ae8db
Randy Oest <oest@amazingrando.com>
2025-02-19 19:56:46 -0500

    lint

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,4 @@
-import { NavPrimary } from "./navPrimary";
+// import { NavPrimary } from "./navPrimary";
 import Randy from '@/assets/images/randy.png'
 import Image from "next/image";
 import Link from "next/link";
